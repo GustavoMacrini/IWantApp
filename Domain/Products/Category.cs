b6279a5466d77f23aa2eed221ff977de04d26bf1
GustavoMacrini <gustavo.godencio@hotmail.com>
2024-02-16 19:09:10 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Products/Category.cs
@@ -0,0 +1,27 @@
+﻿using Flunt.Validations;
+using System.Diagnostics.Contracts;
+
+namespace IWantApp.Domain.Products;
+
+public class Category : Entity
+{
+    public string Name { get; set; }
+    public bool Active { get; set; }
+    public Category(string name, string createdBy, string editedBy)
+    {
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(name, "Name")
+            .IsGreaterOrEqualsThan(name, 3, "Name")
+            .IsNotNullOrEmpty(createdBy, "CreatedBy")
+            .IsNotNullOrEmpty(editedBy, "EditedBy");
+
+        AddNotifications(contract);
+
+        Name = name;
+        Active = true;
+        CreatedBy = createdBy;
+        EditedBy = editedBy;
+        CreatedOn = DateTime.Now;
+        EditedOn = DateTime.Now;
+    }
+}
---
+++ Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,29 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPost
+{
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Hendle => Action;
+
+    public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+
+        var category = new Category(categoryRequest.Name, "Test", "Test");
+
+        if (!category.IsValid)
+        {
+            var erros = category.Notifications
+                .GroupBy(g => g.Key)
+                .ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
+            return Results.ValidationProblem(erros);
+        }
+
+        context.Categories.Add(category);
+        context.SaveChanges();
+        return Results.Created($"/categories/{category.Id}", category.Id);
+    }
+}
