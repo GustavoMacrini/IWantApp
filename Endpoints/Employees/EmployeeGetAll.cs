e63527b9660c5285b19f6011058c5d746b21b118
GustavoMacrini <gustavo.godencio@hotmail.com>
2024-02-18 19:23:21 -0300

    Criando serviço com dapper para gerar response de EmployeeGetAll com paginação - versão melhor performance

--- Endpoints/Employees/EmployeeGetAll.cs
+++ Endpoints/Employees/EmployeeGetAll.cs
@@ -1,29 +1,30 @@
-﻿using IWantApp.Endpoints.Categories;
-using Microsoft.AspNetCore.Identity;
-using System.Security.Claims;
+﻿using IWantApp.Infra.Data;
+using Microsoft.IdentityModel.Tokens;
 
 namespace IWantApp.Endpoints.Employees;
- 
+
 public class EmployeeGetAll
 {
     public static string Template => "/employees";
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Hendle => Action;
 
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    public static IResult Action(int? page, int? rows, QueryAllUsersWithClaimName query)
     {
-
-        var users = userManager.Users.Skip((page -1 ) * rows).Take(rows).ToList();
-        var response = new List<EmployeeResponse>();
-
-        foreach(var item  in users)
+        var errors = new List<string>()
         {
-            var claims = userManager.GetClaimsAsync(item).Result;
-            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
-            var userName = claimName != null ? claimName.Value : string.Empty;
-            response.Add(new EmployeeResponse(item.Email, userName));
+            page == null ? "Page is required" : null,
+            rows == null ? "Rows is required" : null,
+            rows > 5 ? "Rows needs to be equal or less than 5" : null
+        };
+        
+        var errosFiltered = errors.Where(erro => erro != null).ToList();        
+        if (!errosFiltered.IsNullOrEmpty())
+        {
+            return Results.BadRequest(errosFiltered);
         }
 
-        return Results.Ok(response);
+        return Results.Ok(query.Execute(page.Value, rows.Value));
     }
+    
 }
