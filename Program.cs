18561135da795cd9bb59cfd4513ede3d2c48e988
GustavoMacrini <gustavo.godencio@hotmail.com>
2024-02-18 16:05:10 -0300

    Adicionado uso de Claims no endpoint EmployeePost + mudança das restrições de senha

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 using IWantApp.Endpoints.Categories;
+using IWantApp.Endpoints.Employees;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSqlServer<ApplicationDbContext>(
     builder.Configuration["ConnectionString:IWantDb"]);
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequiredLength = 3;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,5 +32,7 @@ app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Hendle)
 app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Hendle);
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Hendle);
 app.MapMethods(CategoryDelete.Template, CategoryDelete.Methods, CategoryDelete.Hendle);
+app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Hendle);
+
 
 app.Run();
